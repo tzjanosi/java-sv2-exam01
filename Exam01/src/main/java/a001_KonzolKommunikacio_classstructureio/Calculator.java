59e5352e0c8e5cc37d9326c8003ddcba5d193b92
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-11-08 23:45:19 +0100

    Init commit

---
+++ Exam01/src/main/java/a001_KonzolKommunikacio_classstructureio/Calculator.java
@@ -0,0 +1,17 @@
+package a001_KonzolKommunikacio_classstructureio;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is the first operand?");
+        int op1 = scanner.nextInt();
+
+        System.out.println("What is the second operand?");
+        int op2 = scanner.nextInt();
+
+        System.out.println(op1+" + " +op2+" = ");
+        System.out.println(op1+op2);
+    }
+}
